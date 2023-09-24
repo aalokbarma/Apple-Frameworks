4bdc2edcbe11805b9a769539d31873cc03d72a5b
Aalok Barma <Aalokbarma17@gmail.com>
2023-09-24 19:30:46 +0530

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Aalok Barma on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
